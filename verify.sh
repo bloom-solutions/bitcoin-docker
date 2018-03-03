43266a4118d03dfb0803fded620fe0b544511447
Ramon Tayag <ramon@tayag.net>
2018-03-03 10:21:05 +0800

    Update verify script with BITCOIN_VERSION instead of hard-coded version

--- verify.sh
+++ verify.sh
@@ -9,6 +9,6 @@ export BITCOIN_ASC=https://bitcoin.org/bin/bitcoin-core-$BITCOIN_VERSION/SHA256S
 curl $BITCOIN_ASC -o SHA256SUMS.asc
 RESULT=$(shasum -a 256 -c SHA256SUMS.asc 2>&1 | grep OK)
 
-if [[ $RESULT != *"bitcoin-0.15.1-x86_64-linux-gnu.tar.gz: OK"* ]]; then
+if [[ $RESULT != *"bitcoin-$BITCOIN_VERSION-x86_64-linux-gnu.tar.gz: OK"* ]]; then
   exit 1;
 fi
